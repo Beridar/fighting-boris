d52963924e68da9aa894c972aa8c5b93384d3889
Robert Norell <robert.norell@outlook.com>
2021-08-05 18:23:02 -0500

    File > New Project.

---
+++ fighting-boris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fighting_boris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
